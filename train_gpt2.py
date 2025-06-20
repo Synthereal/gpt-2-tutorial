99dae3303dfb7b3711b7c1726c8e88545711e1f6
Synthereal <infernofire027@gmail.com>
2025-06-20 03:04:48 -0700

    need to install rocm

--- train_gpt2.py
+++ train_gpt2.py
@@ -131,12 +131,16 @@ class Block(nn.Module):
 
 
 @dataclass
-class GPTconfig:
-    block_size: int = 256
-    vocab_size: int = 65
-    n_layer: int = 6
-    n_head: int = 6
-    n_embd: int = 384
+class GPTConfig:
+    """
+    modeled after hf transformer library
+    """
+    
+    block_size: int = 1024 # max sequence length
+    vocab_size: int = 50257 # number of tokens: 50k BPE merges + 256 hyte tokens + 1 <|endoftext|>
+    n_layer: int = 12 # number of layers
+    n_head: int = 12 # number of heads
+    n_embd: int = 768 # embedding dimension
 
 class GPT(nn.Module):
 
@@ -151,3 +155,130 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+    
+    # allows to generate text
+    def forward(self, idx):
+
+        # idx is shape (B, T)
+        B, T = idx.size()
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is max sequence length."
+
+        # forward token and position embeddings
+        # creates range of indexes from 0 to T, same device as idx
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device) # shape (T)
+        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (T, n_embd)
+        tok_emb = self.transformer.wte(idx) # token embeddings of shape (B, T, n_embd)
+        x = tok_emb + pos_emb # included broadcasting since adding different shape
+
+        # forward blocks to transformer
+        for block in self.transformer.h:
+            x = block(x)
+        x = self.transformer.ln_f(x) # last layer norm
+        # with input (B, T), calculate next token (B, T+1)
+        # vocab_size = number of possible tokens
+        logits = self.lm_head(x) # (B, T, vocab_size)
+        return logits
+
+
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """Loads pretrained GPT-2 model weights from huggingface"""
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        from transformers import GPT2LMHeadModel
+        print("loading weights from pretrained gpt: %s" % model_type)
+
+        # n_layer, n_head, n_embd are determined from model type
+        config_args = {
+            'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
+            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
+            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+        }[model_type]
+        config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
+        config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
+        # create from scratch initialized minGPT model
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer, not a param
+
+        # init a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+        # this means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
+    
+
+
+# -------------------------------------
+
+num_return_sequences = 5
+max_length = 30
+
+model = GPT.from_pretrained('gpt2')
+# good practice put model to evaluation mode when not training
+model.eval() # here it does nothing though
+# TODO: model to ROCm
+model.to('cuda') # i don't have cuda :(
+
+# prefix tokens
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+tokens = enc.encode("Hello, I'm a language mode,")
+tokens = torch.tensor(tokens, dtype=torch.long) # (8, ), encoded is 8 tokens long
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # (5, 8), 5 sequences * 8 tokens
+# looks for the 9th token in each of 5 rows
+x = tokens.to('cuda')
+
+# generate, x = (B, T) = (5, 8)
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+# loop adding new logits (colums of x) until max length is reached
+while x.size(1) < max_length:
+    # forward the model to get logits
+    # no_grad tells pytorch to not prepare for backpropagation, saves memory and space
+    with torch.no_grad():
+        logits = model(x) # (B, T, vocab_size)
+        # take the logits at the last position
+        logits = logits[:, -1, :] # (B, vocab_size), wasteful but correct
+        # get probabilities of next logit
+        probs = F.softmax(logits, dim=-1)
+        # top-k sampling of 50 (huggingface pipeline default)
+        # essentially samples only top 50 probabilities, rest clamp to 0 and renormalize
+        # avoids from going off the rails
+        # topk_probs here becomes (5, 50), topk_indices is (5, 50)
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+        # select token from the top-k probabilities
+        ix = torch.multinomial(topk_probs, 1) # (B, 1)
+        # gather the corresponding indices
+        xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
+        # append to sequence
+        x = torch.cat((x, xcol), dim=1)
+
+# print the generated text
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
